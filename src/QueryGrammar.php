1da9e0b775288791de20b9134330d79938b86a9d
Anthony <anthony@physgun.com>
2025-12-26 15:04:09 -0600

    vibecoded rewrite

--- src/QueryGrammar.php
+++ src/QueryGrammar.php
@@ -37,57 +37,41 @@ class QueryGrammar extends Grammar
     }
 
     /**
-     * Compile WHERE clauses and then rewrite PARAMETER_SIGN markers into :0, :1, ...
+     * Compile WHERE clauses - DON'T rewrite here since this gets called multiple times
      */
     public function compileWheres(Builder $query): string
     {
-        return self::prepareParameters(parent::compileWheres($query));
+        return parent::compileWheres($query);
+    }
+
+    /**
+     * Override compileSelect to do the final parameter rewrite once at the end
+     */
+    public function compileSelect(Builder $query): string
+    {
+        $sql = parent::compileSelect($query);
+        return self::prepareParameters($sql);
     }
 
     /**
      * Replace all occurrences of PARAMETER_SIGN with sequential placeholders.
-     *
-     * IMPORTANT: This function may be called multiple times on the same SQL string
-     * as parts of the query get recompiled. If the SQL already contains :0, :1, etc,
-     * we must continue counting from the highest existing index to avoid collisions.
+     * This should only be called ONCE on the final SQL statement.
      */
     public static function prepareParameters(string $sql): string
     {
-        $next = self::nextParameterIndex($sql);
+        $index = 0;
 
         while (($pos = strpos($sql, self::PARAMETER_SIGN)) !== false) {
-            $sql = substr_replace($sql, ':' . $next, $pos, strlen(self::PARAMETER_SIGN));
-            $next++;
+            $sql = substr_replace($sql, ':' . $index, $pos, strlen(self::PARAMETER_SIGN));
+            $index++;
         }
 
         return $sql;
     }
 
-    /**
-     * Find the next available numeric placeholder index by scanning the SQL.
-     *
-     * Supports:
-     *  - :0, :1, :2 ...
-     */
-    private static function nextParameterIndex(string $sql): int
-    {
-        $max = -1;
-
-        // Match :0, :12, :p0, :p12
-        if (preg_match_all('/:(?:p)?(\d+)/', $sql, $m)) {
-            foreach ($m[1] as $n) {
-                $i = (int) $n;
-                if ($i > $max) {
-                    $max = $i;
-                }
-            }
-        }
-
-        return $max + 1; // if none found => 0
-    }
-
     protected function compileDeleteWithoutJoins(Builder $query, $table, $where): string
     {
-        return "alter table {$table} delete {$where}";
+        $sql = "alter table {$table} delete {$where}";
+        return self::prepareParameters($sql);
     }
 }
